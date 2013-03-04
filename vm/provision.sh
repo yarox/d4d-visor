03d051afb3eeb9fb0baeb1b5a4c082e78e81ee47
Adrian Martin <adrianmartinparra@gmail.com>
2013-03-04 09:03:32 +0100

    Fixed Vagrant environment setup.

--- vm/provision.sh
+++ vm/provision.sh
@@ -1,6 +1,9 @@
 
+# Update the package system
+sudo apt-get -y update
+
 # Add some extra repositories
-sudo apt-get install python-software-properties
+sudo apt-get install -y python-software-properties
 sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
 
 # Update the package system
